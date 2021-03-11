c51e01fe030059763a6b97284db7cfb63aeb0740
Kevin Rise <krise@veracode.com>
2021-03-10 21:00:41 -0500

    Start on Action

--- index.js
+++ index.js
@@ -4,32 +4,45 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-//const fs = require('fs');
 
-import importFlaws from './importer.js'
+const importFlaws = require('./importer').importFlaws;
+//import importFlaws from './importer.js'
 
+// context {{ github.repository }}  = owner/reponame
 
 try {
-    // get input filename, and validate it
+    // get input params
     const resultsFile = core.getInput('pipeline-results-json', {required: true} );
+    const token = core.getInput('github-token', {required: true} );
+    const waitTime = core.getInput('wait-time');
 
+    // other params
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    
 
     //const time = (new Date()).toTimeString();
     //core.setOutput("time", time);
     
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log(`The event payload: ${payload}`);
 
 
         //owner = github.context.repo.owner
     //repo = github.context.repo.repo
 
-    
-    try {
-        importFlaws(resultsFile);
-    }
+    console.log(`Calling with: resultsFile: ${resultsFile}, waitTime: ${waitTime}, owner: ${owner}, repo: ${repo}`)
 
+    // do the thing
+    // importFlaws(
+    //     {resultsFile: resultsFile,
+    //      githubOwner: owner,
+    //      githubRepo: repo,
+    //      githubToken: token,
+    //      waitTime: waitTime}
+    // )
+    // .catch(error => {console.error(`Failure.  ${error.message}`)});
 } catch (error) {
     core.setFailed(error.message);
 }
