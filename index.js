c45c66a08a0b30fa076779909ddd54ce737b4024
Kevin Rise <krise@veracode.com>
2021-03-10 21:03:34 -0500

    cleanup, show token(?)

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const github = require('@actions/github');
 const importFlaws = require('./importer').importFlaws;
 //import importFlaws from './importer.js'
 
-// context {{ github.repository }}  = owner/reponame
-
 try {
     // get input params
     const resultsFile = core.getInput('pipeline-results-json', {required: true} );
@@ -19,20 +17,9 @@ try {
     // other params
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
-    
+    // context {{ github.repository }}  = 'owner/reponame'
 
-    //const time = (new Date()).toTimeString();
-    //core.setOutput("time", time);
-    
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
-
-
-        //owner = github.context.repo.owner
-    //repo = github.context.repo.repo
-
-    console.log(`Calling with: resultsFile: ${resultsFile}, waitTime: ${waitTime}, owner: ${owner}, repo: ${repo}`)
+    console.log(`Calling with: resultsFile: ${resultsFile}, token: ${token}, waitTime: ${waitTime}, owner: ${owner}, repo: ${repo}`)
 
     // do the thing
     // importFlaws(
