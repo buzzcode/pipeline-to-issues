29137b8c5728d82f64261bf12ccc27fc7ce1c2fd
Kevin Rise <krise@veracode.com>
2021-03-11 10:20:44 -0500

    For real, as an Action

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ try {
     // get input params
     const resultsFile = core.getInput('pipeline-results-json', {required: true} );
     const token = core.getInput('github-token', {required: true} );
-    const waitTime = core.getInput('wait-time');
+    const waitTime = core.getInput('wait-time');                // default set in Action.yml
 
     // other params
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
     // context {{ github.repository }}  = 'owner/reponame'
 
-    console.log(`Calling with: resultsFile: ${resultsFile}, token: ${token}, waitTime: ${waitTime}, owner: ${owner}, repo: ${repo}`)
+    //console.log(`Calling with: resultsFile: ${resultsFile}, token: ${token}, waitTime: ${waitTime}, owner: ${owner}, repo: ${repo}`)
 
     // do the thing
-    // importFlaws(
-    //     {resultsFile: resultsFile,
-    //      githubOwner: owner,
-    //      githubRepo: repo,
-    //      githubToken: token,
-    //      waitTime: waitTime}
-    // )
-    // .catch(error => {console.error(`Failure.  ${error.message}`)});
+    importFlaws(
+        {resultsFile: resultsFile,
+         githubOwner: owner,
+         githubRepo: repo,
+         githubToken: token,
+         waitTime: waitTime}
+    )
+    .catch(error => {console.error(`Failure.  ${error.message}`)});
 } catch (error) {
     core.setFailed(error.message);
 }
